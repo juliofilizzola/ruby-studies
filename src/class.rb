0029740fa3bf4342a9af2d0ad367c7ec8ec42dd9
Filizzola <juliofilizzola@hotmail.com>
2022-03-30 11:12:23 -0300

    ⚡️ :  exemplo de class

---
+++ src/class.rb
@@ -0,0 +1,16 @@
+# exemplo de class em Ruby
+class Person
+  def gritar
+    puts "AAAAAAAAAAAAAAAAAAAAAAAAH"
+  end
+
+  def agradecer
+    puts "Obrigado!"
+  end
+end
+
+
+obj = Person.new
+
+obj.gritar
+
